d65f99407b7a4c97e2a70dc588b3f2edcfa239e1
D-mser <serbu.diana.maria@gmail.com>
2021-06-25 17:38:30 +0300

    refactored from class to functional component

--- frontend/src/custom_components/Search.js
+++ frontend/src/custom_components/Search.js
@@ -3,47 +3,45 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
-export default class Search extends React.PureComponent {
-  render() {
-    return (
-      <div id="search">
-        <Autocomplete
-          freeSolo
-          id="free-solo-2-demo"
-          disableClearable
-          options={this.props.data.map((country) => country.name)}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Search input"
-              margin="normal"
-              variant="outlined"
-              InputProps={{ ...params.InputProps, type: "search" }}
-            />
-          )}
-          onChange={(event, value) => {
-            let selectedCountry = this.props.data.filter(
-              (country) => country.name === value
-            )[0];
-            let turf = require("@turf/turf");
-            let polygon = turf.polygon(selectedCountry.location.coordinates);
-            let centroid = turf.centroid(polygon);
-            this.props.handleCenterChange(centroid, value);
-          }}
-        />
-        <Button id="search-button" variant="contained" color="primary">
-          Search
-        </Button>
+export default function Search(props) {
+  return (
+    <div id="search">
+      <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        options={props.data.map((country) => country.name)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search input"
+            margin="normal"
+            variant="outlined"
+            InputProps={{ ...params.InputProps, type: "search" }}
+          />
+        )}
+        onChange={(event, value) => {
+          let selectedCountry = props.data.filter(
+            (country) => country.name === value
+          )[0];
+          let turf = require("@turf/turf");
+          let polygon = turf.polygon(selectedCountry.location.coordinates);
+          let centroid = turf.centroid(polygon);
+          props.handleCenterChange(centroid, value);
+        }}
+      />
+      <Button id="search-button" variant="contained" color="primary">
+        Search
+      </Button>
 
-        <Button
-          id="mark-visited-button"
-          variant="contained"
-          color="primary"
-          onClick={this.props.handleMarkAsVisited}
-        >
-          Mark as Visited
-        </Button>
-      </div>
-    );
-  }
+      <Button
+        id="mark-visited-button"
+        variant="contained"
+        color="primary"
+        onClick={props.handleMarkAsVisited}
+      >
+        Mark as Visited
+      </Button>
+    </div>
+  );
 }
