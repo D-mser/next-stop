525b45f7f54759803380e16153d545911617597c
D-mser <serbu.diana.maria@gmail.com>
2021-07-01 01:10:09 +0300

    added routes and context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,91 +1,28 @@
-import React, { useEffect, useState } from "react";
-import Search from "./custom_components/Search";
-import Mapbox from "./custom_components/Mapbox";
-import axios from "axios";
-import Pannel from "./custom_components/Pannel";
+import React, { useContext } from "react";
+import Home from "./custom_components/Home";
+import MapsBoard from "./custom_components/MapsBoard";
+import Navbar from "./custom_components/Navbar";
+import Login from "./custom_components/Login";
+import { Switch, Route } from "react-router-dom";
+import { DataContext } from "./DataContext";
 
 export default function App() {
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(5);
-  const [markers, setMarkers] = useState([]);
-  const [isFetching, setIsFetching] = useState(true);
-  const [countries, setCountries] = useState([]);
-  const [nextDestination, setNextDestination] = useState([]);
-  const [selectedValue, setSelectedValue] = useState("");
+  const { isFetching } = useContext(DataContext);
 
-  useEffect(() => {
-    if (countries.length) return;
-    axios
-      .get("http://localhost:4000/getCountries")
-      .then((res) => {
-        setCountries((prevCountries) => prevCountries.concat(res.data));
-        setIsFetching(false);
-      })
-      .catch(function (error) {
-        console.log(error);
-        setIsFetching(false);
-      });
-  });
-
-  function handleMarkerUpdate(marker) {
-    setMarkers((prevMarkers) => prevMarkers.concat(marker));
-  }
-
-  function onClearMarkers(map) {
-    for (let index = 0; index < markers.length; index++) {
-      markers[index].remove();
-    }
-
-    setMarkers([]);
-  }
-
-  function handleMapMove(map) {
-    setLng(map.getCenter().lng.toFixed(4));
-    setLat(map.getCenter().lat.toFixed(4));
-    setZoom(map.getZoom().toFixed(2));
-  }
-
-  function handleCenterChange(centroid, selected) {
-    setNextDestination(centroid.geometry.coordinates);
-    setSelectedValue(selected);
-  }
-
-  function handleMarkAsVisited() {
-    const url = "http://localhost:4000/setVisited/" + selectedValue;
-    axios
-      .put(url)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  if (!isFetching) {
-    const visited = countries.filter((country) => country.visited === true);
-    return (
-      <div className="grid-container">
-        <Search
-          data={countries}
-          handleCenterChange={handleCenterChange}
-          handleMarkAsVisited={handleMarkAsVisited}
-        />
-        <Pannel count={visited.length} />
-        <Mapbox
-          lat={lat}
-          lng={lng}
-          zoom={zoom}
-          handleMapMove={handleMapMove}
-          newCenter={nextDestination}
-          visited={visited}
-          handleMarkerUpdate={handleMarkerUpdate}
-          onClearMarkers={onClearMarkers}
-        />
-      </div>
-    );
-  } else {
-    return <div>Almost there</div>;
-  }
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/maps">
+          {!isFetching ? <MapsBoard /> : <div>Almost there</div>}
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </>
+  );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { DataContextProvider } from "./DataContext";
+import { GET_COUNTRIES } from "./config";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <DataContextProvider url={GET_COUNTRIES}>
+      <App />
+    </DataContextProvider>
+  </Router>,
   document.getElementById("root")
 );
